28963974947fc5817b329e1b09d5dcf8ffe811f0
AdinaPepelea <126563079+AdinaPepelea@users.noreply.github.com>
2023-05-04 00:46:51 +0300

    Add files via upload

--- a2/a2.c
+++ a2/a2.c
@@ -8,23 +8,39 @@
 #include <pthread.h>
 
 #define NR_THREADS 4
+#define NR_THREADS1 39
 
 pthread_t tids[NR_THREADS];
-int ok=0;
+pthread_t tids1[NR_THREADS1];
+int ok = 0;
+sem_t sem1;
 
-void *threadFn(void *unused){
-    int arg = *(int *) unused;
-    if((3 == arg || 4 == arg) && ok == 0){
+void *threadFn(void *unused)
+{
+    int arg = *(int *)unused;
+    if (4 == arg && ok==0)
+    {
         info(BEGIN, 3, 4);
         info(BEGIN, 3, 3);
         info(END, 3, 3);
         info(END, 3, 4);
-        ok=1;
+        ok = 1;
     }
-    else if(4 != arg){
+    else if(arg!=3 && arg!=4)
+    {
         info(BEGIN, 3, arg);
         info(END, 3, arg);
     }
+    return NULL;
+}
+
+void *threadFn1(void *unused)
+{
+    int arg = *(int *)unused;
+    sem_wait(&sem1);
+    info(BEGIN, 6, arg);
+    info(END, 6, arg);
+    sem_post(&sem1);
     return NULL;
 }
 
@@ -37,6 +53,9 @@ int main()
     pid_t pid5;
     pid_t pid6;
     pid_t pid7;
+    int ids[4] = {1,2,3,4};
+    int idss[39];
+    sem_init(&sem1, 0, 4);
     info(BEGIN, 1, 0);
     pid2 = fork();
     if (-1 == pid2)
@@ -50,76 +69,108 @@ int main()
         info(END, 2, 0);
         return 0;
     }
-    else{
+    else
+    {
         wait(NULL);
     }
     pid3 = fork();
-    if(-1 == pid3){
+    if (-1 == pid3)
+    {
         perror("Nu s-a creat p3");
         return 1;
     }
-    else if(0 == pid3){
+    else if (0 == pid3)
+    {
         info(BEGIN, 3, 0);
-        for(int i=1;i<=4;i++){
-            pthread_create(&tids[i], NULL, threadFn, &i);
+        for (int i = 0; i < 4; i++)
+        {
+            pthread_create(&tids[i], NULL, threadFn, &ids[i]);
+            
+        }
+        for (int i = 0; i < 4; i++){
             pthread_join(tids[i], NULL);
         }
         pid4 = fork();
-        if(-1 == pid4){
+        if (-1 == pid4)
+        {
             perror("Nu s-a creat p4");
             return 1;
         }
-        else if(0 == pid4){
+        else if (0 == pid4)
+        {
             info(BEGIN, 4, 0);
             pid7 = fork();
-            if(-1 == pid7){
+            if (-1 == pid7)
+            {
                 perror("Nu s-a creat p7");
                 return 1;
             }
-            else if(0 == pid7){
+            else if (0 == pid7)
+            {
                 info(BEGIN, 7, 0);
                 info(END, 7, 0);
                 return 0;
             }
-            else{
+            else
+            {
                 wait(NULL);
             }
             info(END, 4, 0);
             return 0;
         }
-        else{
+        else
+        {
             wait(NULL);
         }
         pid5 = fork();
-        if(-1 == pid5){
+        if (-1 == pid5)
+        {
             perror("Nu s-a creat p5");
             return 1;
         }
-        else if(0 == pid5){
+        else if (0 == pid5)
+        {
             info(BEGIN, 5, 0);
             pid6 = fork();
-            if(-1 == pid6){
+            if (-1 == pid6)
+            {
                 perror("Nu s-a creat p6");
                 return 1;
             }
-            else if(0 == pid6){
+            else if (0 == pid6)
+            {
                 info(BEGIN, 6, 0);
+                sem_init(&sem1, 0, 4);
+                for (int i = 0; i < 39; i++)
+                {
+                    idss[i] = i+1;
+                    pthread_create(&tids1[i], NULL, threadFn1, &idss[i]);
+                    
+                }
+                for (int i = 0; i < 39; i++)
+                {
+                    pthread_join(tids1[i], NULL);
+                }
+                sem_destroy(&sem1);
                 info(END, 6, 0);
                 return 0;
             }
-            else{
+            else
+            {
                 wait(NULL);
             }
             info(END, 5, 0);
             return 0;
         }
-        else{
+        else
+        {
             wait(NULL);
         }
         info(END, 3, 0);
         return 0;
     }
-    else{
+    else
+    {
         wait(NULL);
     }
     info(END, 1, 0);
